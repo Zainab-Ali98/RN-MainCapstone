85807a836596d83ad78413fced3568f2396e3ea6
Zainab-Ali98 <zeeenb1998@gmail.com>
2025-04-08 11:26:54 +0300

    added deposit screen

--- App.js
+++ App.js
@@ -2,20 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthNavigator } from "./navigation/AuthNavigator";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
+import DepositScreen from "./src/screens/DepositScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
-import ParentScreen from "./src/screens/ParentScreen";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
         <View style={styles.container}>
-          <AuthNavigator />
+          {/* <AuthNavigator /> */}
+          <DepositScreen />
           {/* <ProfileScreen/> */}
-          {/* <ParentScreen /> */}
           <StatusBar style="light" />
         </View>
-    </NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../src/screens/LoginScreen";
 import RegisterScreen from "../src/screens/RegisterScreen";
+import DepositScreen from "../src/screens/DepositScreen";
+import ProfileScreen from "../src/screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,8 @@ export const AuthNavigator = () => {
     >
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Deposit" component={DepositScreen} />
     </Stack.Navigator>
   );
 };
