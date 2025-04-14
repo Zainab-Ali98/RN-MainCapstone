edc6ed802ffc69b886049f32a476877a44e2488a
MohammadMeteib <moodxxx84@gmail.com>
2025-04-14 11:56:59 +0300

    Added screens to child nav

--- src/navigation/ChildHomeNavigator.jsx
+++ src/navigation/ChildHomeNavigator.jsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TaskListScreen from "../screens/TaskListScreen";
 import ViewTaskScreen from "../screens/ViewTaskScreen";
 import RewardsScreen from "../screens/RewardsScreen";
+import CreateNewGoal from "../screens/CreateNewGoal";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ export const ChildHomeNavigator = () => {
       <Stack.Screen name="Child" component={TaskListScreen} />
       <Stack.Screen name="TaskProfile" component={ViewTaskScreen} />
       <Stack.Screen name="Reward" component={RewardsScreen} />
+      <Stack.Screen name="CreateNewGoal" component={CreateNewGoal} />
 
     </Stack.Navigator>
   );
